e5c3b9d00a2f0267abb4ab63a5794b35237449c2
tchappel <thomas.chappel4@gmail.com>
2025-02-18 21:27:32 +0100

    convex provider setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+
+const Page = () => {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <main>{tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}</main>
+  );
+};
+
+export default Page;
