e35e13f06f7adc790694ab9f3a508e2d6b181e4d
tchappel <thomas.chappel4@gmail.com>
2025-03-10 21:38:38 +0100

    add members queries and mutations

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,10 +2,55 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
+};
+
+/*
+  # Convention for these files
+
+  ## Queries
+
+  In queries we do not throw errors. just throw default empty states.
+  In queries you can only catch errors with error boundaries
+
+  ## Mutations
+
+  In mutations we throw errors because we can catch them
+*/
+
+// retrieve workspaces the logged in user is member of
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -17,12 +62,19 @@ export const create = mutation({
       throw new Error("User not authenticated");
     }
 
-    // TODO: create proper method to generate a join code
-    const joinCode = "123456";
+    const joinCode = generateCode();
     const workspaceId = await ctx.db.insert("workspaces", {
       name,
       userId,
       joinCode,
+    });
+
+    // every user that creates a workspace will be admin of that workspace
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
     });
 
     return workspaceId;
@@ -36,6 +88,16 @@ export const getById = query({
     if (!userId) {
       throw new Error("User not authenticated");
     }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) return null;
+
     return await ctx.db.get(id);
   },
 });
